bf06b3406fbc65013b974171b1fcf054f1af40f5
abakharev <abakharev@sharevault.com>
2023-04-22 12:50:26 +0300

    store chats in files

--- index.js
+++ index.js
@@ -1,5 +1,14 @@
 import TelegramBot from 'node-telegram-bot-api';
 import { ChatGPTAPI } from 'chatgpt';
+import fs from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
+import { EOL } from 'os';
+
+// eslint-disable-next-line no-underscore-dangle
+const __dirname = path.dirname(fileURLToPath(import.meta.url));
+const chatsDir = path.join(__dirname, 'chats');
+if (!fs.existsSync(chatsDir)) fs.mkdirSync(chatsDir);
 
 const token = '6269585495:AAGkoNZE62dPhYjnGSLcd13Zb5MYgUjleuY';
 const chatGPTAPIKey = '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';
@@ -10,12 +19,87 @@ const api = new ChatGPTAPI({ apiKey: chatGPTAPIKey });
 
 const chats = new Map();
 
+main();
+
+async function main() {
+  await loadChats();
+
+  bot.onText(/.*/, async (msg) => {
+    const { text } = msg;
+    if (!text) return;
+
+    const fromDate = Math.floor((Date.now() - 86400000) / 1000); // 24 hours ago
+    const chatId = msg.chat.id;
+    const messages = getChatMessages(chatId, fromDate);
+
+    // сообщение уже обработано
+    if (messages.some((m) => m.message_id === msg.message_id)) return;
+
+    if (text.includes('/summarize')) {
+      bot.sendMessage(chatId, 'Собираю сообщения за последний день...');
+
+      try {
+        const text = messages.map(getFormattedMessage).join('\n');
+        const summary = await getSummary(text);
+
+        bot.sendMessage(chatId, `Краткая выжимка:\n\n${summary}`);
+      } catch (error) {
+        console.log(error);
+        bot.sendMessage(
+          chatId,
+          'Произошла ошибка при обработке запроса. Пожалуйста, попробуйте еще раз.'
+        );
+      }
+    } else {
+      addMessage(chatId, msg);
+    }
+  });
+
+  console.log('Summarize telegram bot started');
+}
+
+async function loadChats() {
+  const chatsFiles = await fs.promises.readdir(chatsDir);
+  const promises = chatsFiles.map(async (chatIdStr) => {
+    const chatMessagesText = await fs.promises.readFile(path.join(chatsDir, chatIdStr), 'utf-8');
+    const chatMessages = chatMessagesText
+      .split(EOL)
+      .filter((row) => !!row)
+      .map((row) => JSON.parse(row));
+
+    chats.set(Number(chatIdStr), chatMessages);
+  });
+
+  await Promise.all(promises);
+}
+
 function getChatMessages(chatId, fromDate) {
   // eslint-disable-next-line no-return-assign
   let messages = chats.get(chatId) ?? [];
-  messages = messages.filter((message) => message.date >= fromDate);
+  if (fromDate) {
+    messages = messages.filter((message) => message.date >= fromDate);
+  }
   chats.set(chatId, messages);
   return messages;
+}
+
+/**
+ *
+ * @param {import('node-telegram-bot-api').Message} messageRaw
+ */
+function addMessage(chatId, messageRaw) {
+  const message = {
+    message_id: messageRaw.message_id,
+    text: messageRaw.text,
+    from: messageRaw.from,
+    date: messageRaw.date,
+  };
+  const messages = getChatMessages(chatId);
+  messages.push(message);
+  fs.promises.appendFile(
+    path.join(chatsDir, chatId.toString()),
+    `${JSON.stringify(message)}${EOL}`
+  );
 }
 
 function getAuthorName(msg) {
@@ -29,42 +113,15 @@ function getAuthorName(msg) {
   return author.username;
 }
 
+/**
+ * @param {import('node-telegram-bot-api').Message} msg
+ */
 function getFormattedMessage(msg) {
   const authorName = getAuthorName(msg);
+  // const text = '(' + msg.date + ')';
   if (authorName) return `${authorName}: ${msg.text}`;
   return msg.text;
 }
-
-bot.onText(/.*/, async (msg) => {
-  const { text } = msg;
-  if (!text) return;
-
-  const fromDate = Math.floor((Date.now() - 86400000) / 1000); // 24 hours ago
-  const messages = getChatMessages(msg.chat.id, fromDate);
-
-  if (text.includes('/summarize')) {
-    const chatId = msg.chat.id;
-
-    bot.sendMessage(chatId, 'Собираю сообщения за последний день...');
-
-    try {
-      const text = messages.map(getFormattedMessage).join('\n');
-      const summary = await getSummary(text);
-
-      bot.sendMessage(chatId, `Краткая выжимка:\n\n${summary}`);
-    } catch (error) {
-      console.log(error);
-      bot.sendMessage(
-        chatId,
-        'Произошла ошибка при обработке запроса. Пожалуйста, попробуйте еще раз.'
-      );
-    }
-  } else {
-    messages.push(msg);
-  }
-});
-
-console.log('Summarize telegram bot started');
 
 async function getSummary(text) {
   const maxLength = 3500;
@@ -73,7 +130,7 @@ async function getSummary(text) {
 
   for (const part of textParts) {
     const response = await api.sendMessage(
-      `Сделай краткую выжимку на русском из этих сообщений в виде 5 пунктов:\n${part}`,
+      `Сделай краткую выжимку на русском из этих сообщений в виде 5 пунктов идущих в хронологическом порядке:\n${part}`,
       {
         completionParams: { max_tokens: 2048 },
       }
