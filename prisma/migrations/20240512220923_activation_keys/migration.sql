5206e9d2f4ace4164bbf45392f1b2a97c84db589
abakharev <artyom.bakharev@gmail.com>
2024-05-13 01:47:18 +0300

    feat: activation keys

--- prisma/migrations/20240512220923_activation_keys/migration.sql
+++ prisma/migrations/20240512220923_activation_keys/migration.sql
@@ -1,9 +1,9 @@
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
 INSERT INTO
-  "ActivationKey" ("id", "tariffId")
-VALUES
-  (uuid_generate_v4(), '+100s'),
-  (uuid_generate_v4(), '+20s'),
-  (uuid_generate_v4(), '+20s,x2'),
-  (uuid_generate_v4(), 'x2');
+  "ActivationKey" (id, "tariffId")
+SELECT
+  uuid_generate_v4(),
+  id
+FROM
+  "Tariff";
